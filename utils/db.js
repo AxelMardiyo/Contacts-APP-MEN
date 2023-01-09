dad16a69b47b5220bd2f7d610b89bedad7b7ff4e
AxelMardiyo <118111219+AxelMardiyo@users.noreply.github.com>
2023-01-09 21:45:13 +0700

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose")
-const uri = "mongodb+srv://AxelCode:<password>@cluster0.sjievuu.mongodb.net/?retryWrites=true&w=majority"
+const uri = "mongodb+srv://AxelCode:AxelCode@cluster0.sjievuu.mongodb.net/?retryWrites=true&w=majority"
 mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
