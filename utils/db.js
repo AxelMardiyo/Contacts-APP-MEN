58ecc975400f5722242213716bc0c934719fce02
AxelMardiyo <118111219+AxelMardiyo@users.noreply.github.com>
2023-01-09 21:43:29 +0700

    Update db.js

--- utils/db.js
+++ utils/db.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose")
-mongoose.connect("mongodb://127.0.0.1:27017/ContactsAPP", {
+const uri = "mongodb+srv://AxelCode:<password>@cluster0.sjievuu.mongodb.net/?retryWrites=true&w=majority"
+mongoose.connect(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
